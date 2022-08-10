0b720aad25271a7999e4cda7ae695d1c89a80782
steinily <henczistvan87@gmail.com>
2022-08-10 18:04:52 +0200

    update

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,23 @@
 const http = require('http');
 const app = require('./app');
+const server = http.createServer(app);
 const { loadPlanetsData, } = require('./models/planets.models')
-<<<<<<< Updated upstream
+
 const { mongoConnecnt } = require('./services/mongo')
 
 const PORT = process.env.PORT || 8000;
-=======
+
 const mongoose=require('mongoose')
 
-let MONGO_URL="mongodb://steinily:samsung@172.21.0.1:8080/"
-
-const PORT = process.env.PORT || 8000;
+let MONGO_URL="mongodb+srv://admin:ZCsuaTRe4CUKWhoM@cluster0.l9bne.mongodb.net/?retryWrites=true&w=majority"
 
 
-
->>>>>>> Stashed changes
-const server = http.createServer(app);
 
 
 async function startServer(){
-<<<<<<< Updated upstream
+
     await mongoConnecnt();    
-=======
     await mongoose.connect(MONGO_URL)
->>>>>>> Stashed changes
     await loadPlanetsData();
     server.listen(PORT , () => {
         console.log(` Listening on port ${PORT}...`)
